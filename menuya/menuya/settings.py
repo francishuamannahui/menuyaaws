f2e506e0dfc70210eebd74146dfcaaddbd6394dd
francishuamannahui <44727021+francishuamannahui@users.noreply.github.com>
2021-11-30 21:43:28 -0500

    Progres

--- menuya/menuya/settings.py
+++ menuya/menuya/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'menuya.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'menuya',
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': 'menuyaadm',
+        'PASSWORD': 'Fr@nz0011',
         'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'PORT': '5432',
     }
 }
 
